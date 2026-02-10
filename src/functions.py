a5c3996b84e832ebd5ae3c91819c6861ad6f52c5
Manuel-Knepper <manuel.knepper@gmx.net>
2026-02-10 10:49:22 +0100

    added get_seed_points function to functions.py and included it in GrowBikeMVP.ipynb

--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,8 @@
+import numpy as np
+import geopandas as gpd
+from shapely.prepared import prep
+from shapely.geometry import Point
+
 # helper function to check whether newly to be added edge intersects with already added edges
 def intersects_properly(geom1, geom2):
     '''
@@ -18,3 +23,29 @@ def get_correct_edgetuples(edge_gdf, nodelist):
         else:
             edgelist_final.append(tuple([edge_prelim[1], edge_prelim[0]]))
     return edgelist_final
+
+# create seed points for greedy triangulation
+def get_seed_points (edges, proj_crs, seed_point_spacing):
+    # get convex hull around edge area
+    hull = edges.union_all().convex_hull
+    # get bounds of hull
+    latmin, lonmin, latmax, lonmax = hull.bounds
+
+    # https://stackoverflow.com/questions/66010964/fastest-way-to-produce-a-grid-of-points-that-fall-within-a-polygon-or-shape
+    # populate hull bbox with evenly spaced seeding points
+    points = []
+    for lat in np.arange(latmin, latmax, seed_point_spacing):
+        for lon in np.arange(lonmin, lonmax, seed_point_spacing):
+            points.append(Point((round(lat, 4), round(lon, 4))))
+
+    # keep only those seed points that are within the hull polygon
+    prep_polygon = prep(hull)
+    valid_points = []
+    valid_points.extend(filter(prep_polygon.contains, points))
+
+    # store seed points in gdf
+    seed_points = gpd.GeoDataFrame(
+        {"geometry": valid_points},
+        crs=proj_crs
+    )
+    return seed_points
