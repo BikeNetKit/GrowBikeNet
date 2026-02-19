848f51c69599e64fb883bc9ed8c0f510fc8d2ef2
Manuel-Knepper <manuel.knepper@gmx.net>
2026-02-19 11:42:37 +0100

    added test_triangulation_creation to test_functions.py

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -32,7 +32,6 @@ def method():
 def test_rank(create_test_data_rank, method, create_validation_data_rank):
     assert_frame_equal(rank_df(create_test_data_rank, method), create_validation_data_rank, check_dtype=False)
 
-
 @pytest.fixture
 def define_seed_point_delta():
     return 500
@@ -54,3 +53,16 @@ def create_filtered_seed_points():
 
 def test_seed_point_filter(create_snapped_seed_points, create_filtered_seed_points, define_seed_point_delta):
     assert_frame_equal(filter_seed_points(create_snapped_seed_points, define_seed_point_delta), create_filtered_seed_points, check_dtype=False)
+
+@pytest.fixture
+def create_validation_triangulation():
+    linestring = LineString([Point(1001,1001), Point(3001, 3001)])
+    pair = '1', '3'
+    d = {'pair': [list(pair)], 'potential_edge': [linestring], 'dist': [linestring.length]}
+    df = pd.DataFrame(d)
+    return df
+
+def test_triangulation_creation(create_validation_triangulation, create_filtered_seed_points):
+    assert_frame_equal(create_potential_triangulation(create_filtered_seed_points), create_validation_triangulation, check_dtype=False)
+
+#def test_triangulation_filter():
