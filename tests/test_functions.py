4229ef0d60848019441676d77dadc29a6b602d10
Michael Szell <michael@szell.net>
2026-05-19 14:46:12 +0200

    Add test_remove_edge_overlaps, fix function

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -9,34 +9,35 @@ from growbikenet.functions import (
     filter_seed_points,
     rank_df,
     intersects_properly,
+    remove_edge_overlaps,
 )
-from shapely.geometry import Point, LineString
+from shapely.geometry import Point, LineString, MultiLineString
 
 
 @pytest.fixture
-def create_geom_1():
+def geom_1():
     linestring = LineString([(0, 0), (1, 1), (2, 2)])
     return linestring
 
 
 @pytest.fixture
-def create_geom_2():
+def geom_2():
     linestring = LineString([(3, 3), (4, 4), (5, 5)])
     return linestring
 
 
-def test_intersects_properly(create_geom_1, create_geom_2):
-    assert intersects_properly(create_geom_1, create_geom_2) is False
+def test_intersects_properly(geom_1, geom_2):
+    assert intersects_properly(geom_1, geom_2) is False
 
 
 @pytest.fixture
-def create_test_data_rank(method):
+def test_data_rank(method):
     df = pd.DataFrame((6, 2, 4), columns=[method])
     return df
 
 
 @pytest.fixture
-def create_validation_data_rank(method):
+def validation_data_rank(method):
     ranked_df = pd.DataFrame(
         ([6, 0], [4, 1], [2, 2]), columns=[method, "ordering_betweenness_centrality"]
     )
@@ -48,21 +49,21 @@ def method():
     return "betweenness_centrality"
 
 
-def test_rank_df(create_test_data_rank, method, create_validation_data_rank):
+def test_rank_df(test_data_rank, method, validation_data_rank):
     assert_frame_equal(
-        rank_df(create_test_data_rank, method),
-        create_validation_data_rank,
+        rank_df(test_data_rank, method),
+        validation_data_rank,
         check_dtype=False,
     )
 
 
 @pytest.fixture
-def define_seed_point_delta():
+def seed_point_delta():
     return 500
 
 
 @pytest.fixture
-def create_snapped_seed_points():
+def snapped_seed_points():
     d = {
         "osmid": ["1", "2", "3"],
         "geometry_generated": [Point(1000, 1000), Point(2000, 2000), Point(3000, 3000)],
@@ -75,7 +76,7 @@ def create_snapped_seed_points():
 
 
 @pytest.fixture
-def create_filtered_seed_points():
+def filtered_seed_points():
     d = {"osmid": ["1", "3"], "geometry": [Point(1001, 1001), Point(3001, 3001)]}
     gdf = gpd.GeoDataFrame(d, geometry="geometry", crs="EPSG:3857")
     gdf = gdf.set_index("osmid")
@@ -85,17 +86,17 @@ def create_filtered_seed_points():
 
 
 def test_filter_seed_points(
-    create_snapped_seed_points, create_filtered_seed_points, define_seed_point_delta
+    snapped_seed_points, filtered_seed_points, seed_point_delta
 ):
     assert_frame_equal(
-        filter_seed_points(create_snapped_seed_points, define_seed_point_delta),
-        create_filtered_seed_points,
+        filter_seed_points(snapped_seed_points, seed_point_delta),
+        filtered_seed_points,
         check_dtype=False,
     )
 
 
 @pytest.fixture
-def create_validation_streets():
+def validation_streets():
     streets_nodes = gpd.read_file(
         "./tests/test_data/oelde_streets.gpkg", layer="nodes"
     ).set_index("osmid")
@@ -106,22 +107,55 @@ def create_validation_streets():
     return streets
 
 
-def test_get_principal_bearing(create_validation_streets):
-    assert get_principal_bearing(create_validation_streets) == 65.0
+def test_get_principal_bearing(validation_streets):
+    assert get_principal_bearing(validation_streets) == 65.0
 
 
 @pytest.fixture
-def create_validation_grid():
+def validation_grid():
     grid = gpd.read_file("./tests/test_data/oelde_grid.gpkg")
     return grid
 
 
-def test_get_grid_seed_points(create_validation_grid, create_validation_streets):
+def test_get_grid_seed_points(validation_grid, validation_streets):
     edges = ox.convert.graph_to_gdfs(
-        create_validation_streets,
+        validation_streets,
         nodes=False,
         edges=True,
         node_geometry=False,
         fill_edge_geometry=False,
     )
-    create_validation_grid.equals(get_grid_seed_points(edges, 1707, 65.0))
+    validation_grid.equals(get_grid_seed_points(edges, 1707, 65.0))
+
+
+@pytest.fixture
+def ordered_edges():
+    d = {"geometry": [
+    MultiLineString([((1,4), (2,4)), ((2,2), (3,2)), ((4,2), (6,2))]), # existing bike network
+    LineString([(1,2), (3,2)]),
+    LineString([(5,2), (7,2)]),
+    LineString([(5,1), (5,2), (6,2), (6,4)]),
+    LineString([(4,2), (7,2)]),
+    LineString([(3,4), (4,4)])
+     ]}
+    gdf = gpd.GeoDataFrame(d, geometry="geometry", crs="EPSG:3857")
+    return gdf
+
+@pytest.fixture
+def ordered_edges_without_overlaps():
+    d = {"geometry": [
+    MultiLineString([((1,4), (2,4)), ((2,2), (3,2)), ((4,2), (6,2))]), # existing bike network
+    LineString([(1,2), (2,2)]),
+    LineString([(6,2), (7,2)]),
+    MultiLineString([((5,1), (5,2)), ((6,2), (6,4))]),
+    LineString([(3,4), (4,4)])
+     ]}
+    gdf = gpd.GeoDataFrame(d, geometry="geometry", crs="EPSG:3857")
+    return gdf
+
+def test_remove_edge_overlaps(ordered_edges, ordered_edges_without_overlaps):
+    assert_frame_equal(
+        remove_edge_overlaps(ordered_edges),
+        ordered_edges_without_overlaps,
+        check_dtype=False,
+    )
