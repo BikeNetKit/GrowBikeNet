0b7179f02cb89caffdd6a0d22de0008f3801df27
Michael Szell <michael@szell.net>
2026-05-23 20:46:51 +0200

    Change theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ html_static_path = ["_static"]
 
 ### select html theme
 # html_theme = 'alabaster'
-html_theme = "pydata_sphinx_theme"
+html_theme = "furo"
 
 html_theme_options = {
     "github_url": "https://github.com/BikeNetKit/GrowBikeNet",
