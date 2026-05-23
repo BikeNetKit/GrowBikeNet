5fc794e4a37335827c1f94ec5313fc6defe5ffc8
Michael Szell <michael@szell.net>
2026-05-23 23:05:15 +0200

    Fix project name capitalization

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "GrowBikeNet"
+project = "growbikenet"
 copyright = "2026, GrowBikeNet developers"
 author = "Szell, Vybornova, Knepper"
 
