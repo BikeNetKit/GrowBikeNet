5ac50cd957af1d6e481c8cfb6744fb4fa52464eb
Michael Szell <michael@szell.net>
2026-06-08 17:21:35 +0200

    Test fail cases of no rail stations or bike net

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,6 @@
 import pytest
 import geopandas as gpd
+import osmnx as ox
 from growbikenet.growbikenet import growbikenet
 
 
@@ -8,8 +9,7 @@ def create_validation_gdf():
     gdf = gpd.read_file("./tests/test_data/oelde_growbikenet.gpkg", layer='Grown bike network')
     return gdf
 
-
-def test_growbikenet(create_validation_gdf):
+def test_growbikenet_case_success(create_validation_gdf):
     create_validation_gdf.equals(
         growbikenet(
             city_name="Oelde",
@@ -20,3 +20,23 @@ def test_growbikenet(create_validation_gdf):
             export_video=False,
         )
     )
+
+def test_growbikenet_case_fail_rail():
+    """Verify that in the absence of rail stations (Andorra), an 
+    osmnx._errors.InsufficientResponseError is thrown.
+    """
+    with pytest.raises(ox._errors.InsufficientResponseError):
+        growbikenet(
+            "Andorra",
+            seed_point_type='rail',
+        )
+
+def test_growbikenet_case_fail_existing_network():
+    """Verify that in the absence of an existing bike network (Hallettsville), an 
+    osmnx._errors.InsufficientResponseError is thrown.
+    """
+    with pytest.raises(ox._errors.InsufficientResponseError):
+        growbikenet(
+            "Hallettsville",
+            existing_network_spacing=500,
+        )
