6882a8f426a2928d8533d5748a43891339e90193
Michael Szell <michael@szell.net>
2026-06-29 21:03:49 +0200

    Add docstrings to constants and settings

--- growbikenet/constants.py
+++ growbikenet/constants.py
@@ -1,10 +1,19 @@
 '''
 Global constants for growbikenet that can be tweaked during development, but should not be changed later by the user.
 
+PBI_CUSTOM_FILTER : list
+    Custom filter for protected bicycle infrastructure (pbi)
+PRESET_TAGS : dict
+    Pre-defined tags to select tags as seed points
+PHI_LIMITS : list
+    Two orientation order limits between street networks with:
+    1) negligible grid elements, 2) some grid elements, 3) grid.
+    We aimed to use the tercile limits from the paper [3]_ (Fig 2), but the values here are lower for unknown reasons, also with the unweighted version. Also, it was aimed to have Barcelona in the grid category. For these reasons, the limits were lowered.
+EXISTING_NETWORK_MINIMUM_COMPONENT_LENGTH : int
+    Minimum length a bike network component needs to have for seed points to snap
 '''
 
 
-# Custom filter for protected bicycle infrastructure (pbi)
 PBI_CUSTOM_FILTER = ['["cycleway"~"track"]',
           '["highway"~"cycleway"]',
           '["highway"~"path"]["bicycle"~"designated"]',
@@ -23,21 +32,12 @@ for custom_tag in ["cycleway", "bicycle", "cycleway:right", "cycleway:left", "cy
     if custom_tag not in ox.settings.useful_tags_way:
         ox.settings.useful_tags_way.extend(custom_tag)
 
-
-# Pre-defined tags to select tags as seed points
 PRESET_TAGS = {
             "rail": {"railway": ["station", "halt"]},
             "school": {"amenity": ["kindergarten", "school", "college", "university"]},
             "park": {"leisure": ["park", "garden", "nature_reserve", "bathing_place"]},
             }
 
-
-# Orientation order limits between street networks with:
-# 1) negligible grid elements, 2) some grid elements, 3) grid.
-# We aimed to use the tercile limits from the paper [3]_ (Fig 2), but the values
-# here are lower for unknown reasons, also with the unweighted version. Also, it was aimed to have Barcelona in the grid category. For these reasons, the limits were lowered.
 PHI_LIMITS = [0.02, 0.08] # Tercile limits in the paper: 0.033, 0.161
 
-
-# Minimum length a bike network component needs to have for seed points to snap
 EXISTING_NETWORK_MINIMUM_COMPONENT_LENGTH = 100 
--- growbikenet/settings.py
+++ growbikenet/settings.py
@@ -1,19 +1,21 @@
 '''
 Global settings for growbikenet that can be configured by the user.
 
-Parameters
-----------
-    crs_projected : str, default '3857'
-        EPSG code of the coordinate reference system that is used to project osm data. Default is '3857' (WGS 84 / Pseudo-Mercator). If this web mercator projection is not needed, then for Europe '3035' (LAEA) and globally '54035' (Equal Earth) is better.
+export_path : dict(str | Path)
+    Paths to results, plots, and video folders to save data, plots, and videos. 
+import_path : str | Path
+    Path to import files (as defined in growbikenet's import_files parameter).
+crs_projected : str, default '3857'
+    EPSG code of the coordinate reference system that is used to project osm data. Default is '3857' (WGS 84 / Pseudo-Mercator). If this web mercator projection is not needed, then for Europe '3035' (LAEA) and globally '54035' (Equal Earth) is better.
 
 '''
 
 
 
 export_path = {
-"results":"./results/",
-"plots":"./results/plots/",
-"videos":"./results/videos/",
+    "results":"./results/",
+    "plots":"./results/plots/",
+    "videos":"./results/videos/",
 }
 
 import_path = "./"
