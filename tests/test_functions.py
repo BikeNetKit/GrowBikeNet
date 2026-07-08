07706600660f4fcd67a74cc1f27711626fef3563
pess <147063686+marianamirandapessoa@users.noreply.github.com>
2026-07-08 11:41:13 +0200

    Add simple test case of add_trip_data_to_net()

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -10,6 +10,7 @@ from growbikenet.functions import (
     rank_df,
     # intersects_properly,
     remove_edge_overlaps,
+    add_trip_data_to_net
 )
 from shapely.geometry import Point, LineString, MultiLineString
 
@@ -158,4 +159,149 @@ def test_remove_edge_overlaps(ordered_edges, ordered_edges_without_overlaps):
         remove_edge_overlaps(ordered_edges),
         ordered_edges_without_overlaps,
         check_dtype=False,
+    )
+
+
+@pytest.fixture
+def node_seed_points():
+    # Nodes of the graph
+    A = (800,1800)
+    B = (1300,1800)
+    C = (1800,1800)
+    D = (800,1300)
+    E = (1300,1300)
+    F = (1800,1300)
+    G = (800,800)
+    H = (1300,800)
+
+    d = {
+        'osmid': [1, 2, 3, 4, 5, 6, 7, 8],
+        'geometry':[Point(A), Point(B), Point(C), Point(D), Point(E), Point(F), Point(G), Point(H)]
+    }
+
+    nodes = gpd.GeoDataFrame(d, geometry='geometry', crs = "EPSG:3857")
+    nodes = nodes.set_index('osmid')
+
+    return nodes
+
+@pytest.fixture
+def routed_edges():
+    # Nodes of the graph
+    A = (800,1800)
+    B = (1300,1800)
+    C = (1800,1800)
+    D = (800,1300)
+    E = (1300,1300)
+    F = (1800,1300)
+    G = (800,800)
+    H = (1300,800)
+
+    # Auxiliary nodes
+    I = (1050,1950)
+    J = (1340,1700)
+    K = (900,1250)
+    L = (1500,1250)
+    M = (1150,1100)
+
+    e = {
+        'pair': [(1,2), (1,4), (2,3), (2,5), (3,6), (4,5), (4,7), (5,6), (5,8), (6,8), (7,8)],
+        'dist': [583.10, 500, 500, 509.70, 500, 514.92, 500, 510.29, 585.41, 707.11, 500],
+        'geometry': [
+            LineString([A,I,B]),
+            LineString([A,D]),
+            LineString([B,C]),
+            LineString([B,J,E]),
+            LineString([C,F]),
+            LineString([D,K,E]),
+            LineString([D,G]),
+            LineString([E,L,F]),
+            LineString([E,M,H]),
+            LineString([F,H]),
+            LineString([G,H]),
+        ]
+    }
+    edges = gpd.GeoDataFrame(e, geometry="geometry", crs = "EPSG:3857")
+
+    return edges
+
+@pytest.fixture
+def trips_data():
+    # Points from trips data          In ESPG:3857
+    O0 = Point(0.014499, 0.022)          # (1614,2449)
+    D0 = Point(0.016969, 0.015074)       # (1889,1678)
+    O1 = Point(0.015891, 0.009459)       # (1769,1053)
+    D1 = Point(0.009226, 0.016502)       # (1027,1837)
+    O2 = Point(0.007744,0.013376)        # (862,1489)
+    D2 = Point(0.006558,0.011768)        # (730,1310)
+    O3 = Point(0.014624, 0.012019)       # (1628,1338)
+    D3 = Point(0.006576, 0.01325)        # (732,1475)
+    O4 = Point(0.009549, 0.013906)       # (1063,1548)
+    D4 = Point(0.010034, 0.006728)       # (1117,749)
+    O5 = Point(0.009388, 0.009675)       # (1045,1077)
+    D5 = Point(0.009432,0.007519)        # (1050,837)
+    O6 = Point(0.007537, 0.00928)        # (839,1033)
+    D6 = Point(0.012011,0.013888)        # (1337,1546)
+        
+
+    t = {
+        'o_lat': [O0.y, O1.y, O2.y, O3.y, O4.y, O5.y, O6.y],
+        'o_lon': [O0.x, O1.x, O2.x, O3.x, O4.x, O5.x, O6.x],
+        'd_lat': [D0.y, D1.y, D2.y, D3.y, D4.y, D5.y, D6.y],
+        'd_lon': [D0.x, D1.x, D2.x, D3.x, D4.x, D5.x, D6.x],
+        'num': [1, 1, 1, 2, 1, 1, 1]
+    }
+
+    trips = pd.DataFrame(t)
+
+    return trips
+
+@pytest.fixture
+def routed_edges_with_data():
+    # Nodes of the graph
+    A = (800,1800)
+    B = (1300,1800)
+    C = (1800,1800)
+    D = (800,1300)
+    E = (1300,1300)
+    F = (1800,1300)
+    G = (800,800)
+    H = (1300,800)
+
+    I = (1050,1950)
+    J = (1340,1700)
+    K = (900,1250)
+    L = (1500,1250)
+    M = (1150,1100)
+
+    e = {
+        'pair': [(1,2), (1,4), (2,3), (2,5), (3,6), (4,5), (4,7), (5,6), (5,8), (6,8), (7,8)],
+        'dist': [583.10, 500, 500, 509.70, 500, 514.92, 500, 510.29, 585.41, 707.11, 500],
+        'num_trips': [0, 1, 0, 0, 0, 4, 2, 3, 1, 0, 0],
+        'geometry': [
+            LineString([A,I,B]),
+            LineString([A,D]),
+            LineString([B,C]),
+            LineString([B,J,E]),
+            LineString([C,F]),
+            LineString([D,K,E]),
+            LineString([D,G]),
+            LineString([E,L,F]),
+            LineString([E,M,H]),
+            LineString([F,H]),
+            LineString([G,H]),
+        ]
+    }
+    edges = gpd.GeoDataFrame(e, geometry="geometry", crs = "EPSG:3857")
+
+    return edges
+
+def test_add_trip_data_to_net_case_success_simple(trips_data, node_seed_points, routed_edges, routed_edges_with_data):
+    assert_frame_equal(
+        add_trip_data_to_net(
+            trips_data, 
+            node_seed_points,
+            routed_edges,
+            '3857'
+        ),
+        routed_edges_with_data
     )
