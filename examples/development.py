3fee6c5f30c85887820d4ae5be6dad31e927f106
Michael Szell <michael@szell.net>
2026-07-20 13:06:19 +0200

    Add usage_03 content

--- examples/development.py
+++ examples/development.py
@@ -3,7 +3,5 @@
 import growbikenet as gbn
 
 edges_ranked = gbn.growbikenet("Municipality of Athens",
-                                seed_point_linking="triangulate_delaunay",
-                              existing_network_spacing=500,
-                              export_file_format="gpkg")
+                              existing_network_spacing='auto',)
 
