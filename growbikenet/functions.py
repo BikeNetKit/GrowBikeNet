78c716065e63f24661ecbf6ae52c2dff5957b908
pess <147063686+marianamirandapessoa@users.noreply.github.com>
2026-07-23 19:57:18 +0200

    Add internal checkpoint in add_point_data_to_net()

--- growbikenet/functions.py
+++ growbikenet/functions.py
@@ -284,6 +284,11 @@ def add_point_data_to_net(points, edges, crs_projected, matching_distance=500):
             edge = np.sort(pos[1][nearest_edges_ids])[0]
             edges_with_data.at[edge, "num_points"] += num
 
+    # Checks for pytest
+    print(f"Nearest edges to point 31: {rtree.query_nearest(points_geoms[31])}")
+    print(f"Nearest edges to point 33: {rtree.query_nearest(points_geoms[33])}")
+    print(f"Nearest edges to point 332: {rtree.query_nearest(points_geoms[332])}")
+
     # Move the 'geometry' column to the end of the GeoDataFrame
     # Necessary for testing the function
     cols = [c for c in edges_with_data.columns if c != 'geometry'] + ['geometry']
