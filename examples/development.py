559a67a246d0817f7bc626670a815a012c173e17
Michael Szell <michael@szell.net>
2026-08-05 16:36:17 +0200

    Add test cases for Turin with import data

--- examples/development.py
+++ examples/development.py
@@ -2,10 +2,14 @@
 
 import growbikenet as gbn
 
+gbn.settings.import_data_impact = 9
+gbn.settings.import_data_trip_point_balance = 0.4 # alpha
 edges_ranked = gbn.growbikenet("Turin", 
+    export_data = True,
     import_files={
         'street_network':"./tests/test_data/turin_street_network.gpkg",
         'point_data':"./tests/test_data/turin_crashes.gpkg",
         'trip_data':"./tests/test_data/turin_trips.csv",
         },
     )
+print(len(edges_ranked))
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
 import pytest
 import geopandas as gpd
 import osmnx as ox
-from growbikenet.growbikenet import growbikenet
+import growbikenet as gbn
+from pandas.testing import assert_frame_equal
 
 @pytest.fixture
 def validation_gdf_oelde():
@@ -23,7 +24,7 @@ def test_growbikenet_case_success_online(validation_gdf_oelde):
     This test might brake whenever Oelde is changed too much on OSM!
     """
     validation_gdf_oelde.equals(
-        growbikenet(
+        gbn.growbikenet(
             city_query="Oelde",
             ranking="betweenness_centrality",
             export_data=False,
@@ -34,7 +35,7 @@ def test_growbikenet_case_success_offline1(validation_gdf_oelde):
     """Verify that the offline version of growbikenet works as intended.
     """
     validation_gdf_oelde.equals(
-        growbikenet(
+        gbn.growbikenet(
             city_query="Oelde",
             ranking="betweenness_centrality",
             export_data=False,
@@ -46,7 +47,7 @@ def test_growbikenet_case_success_offline2(validation_gdf_athens):
     """Verify that the offline version of growbikenet works as intended, with existing bike network.
     """
     validation_gdf_athens.equals(
-        growbikenet(
+        gbn.growbikenet(
             city_query="Municipality of Athens",
             ranking="betweenness_centrality",
             export_data=False,
@@ -70,7 +71,7 @@ def test_growbikenet_case_success_offline3(validation_gdf_asti):
     >>> ox.io.save_graph_geopackage(g, "asti_street_network.gpkg")
     """
     validation_gdf_asti.equals(
-        growbikenet(
+        gbn.growbikenet(
             city_query="Asti 16", # The 16 is needed to select the city, not the municipality (it doesn't matter here as the network is imported anyway, but it is important to know for exporting the street network asti_street_network.gpkg in the first place)
             ranking="betweenness_centrality",
             export_data=False,
@@ -83,7 +84,7 @@ def test_growbikenet_case_fail_rail1():
     RunTimeError is thrown.
     """
     with pytest.raises(Exception):
-        growbikenet(
+        gbn.growbikenet(
             "Oelde",
             seed_point_type='rail',
         )
@@ -93,7 +94,7 @@ def test_growbikenet_case_fail_rail2():
     osmnx._errors.InsufficientResponseError is thrown.
     """
     with pytest.raises(ox._errors.InsufficientResponseError):
-        growbikenet(
+        gbn.growbikenet(
             "Andorra",
             seed_point_type='rail',
         )
@@ -103,7 +104,73 @@ def test_growbikenet_case_fail_existing_network():
     osmnx._errors.InsufficientResponseError is thrown.
     """
     with pytest.raises(ox._errors.InsufficientResponseError):
-        growbikenet(
+        gbn.growbikenet(
             "Hallettsville",
             existing_network_spacing=500,
         )
+
+
+
+# Tests with settings
+
+
+@pytest.fixture
+def validation_gdf_turin_alpha1():
+    gdf = gpd.read_file("./tests/test_data/turin_alpha1.gpkg", layer='Grown bike network')
+    return gdf
+
+@pytest.fixture
+def validation_gdf_turin_alpha0_4():
+    gdf = gpd.read_file("./tests/test_data/turin_alpha0_4.gpkg", layer='Grown bike network')
+    return gdf
+
+def test_growbikenet_case_success_offline_import_data1(validation_gdf_turin_alpha1):
+    """Verify that the offline version of growbikenet works as intended with import data sets.
+    """
+    gbn.settings.import_data_impact = 9
+    gbn.settings.import_data_trip_point_balance = 1 # alpha
+
+    validation_gdf_turin_alpha1.equals(
+        gbn.growbikenet(
+            "Turin", 
+            import_files={
+                'street_network':"./tests/test_data/turin_street_network.gpkg",
+                'point_data':"./tests/test_data/turin_crashes.gpkg",
+                'trip_data':"./tests/test_data/turin_trips.csv",
+            },
+        )
+    )
+
+def test_growbikenet_case_success_offline_import_data2(validation_gdf_turin_alpha1):
+    """Verify that the offline version of growbikenet works as intended with import data sets.
+    """
+    gbn.settings.import_data_impact = 9
+    gbn.settings.import_data_trip_point_balance = 1 # alpha
+    
+    validation_gdf_turin_alpha1.equals(
+        gbn.growbikenet(
+            "Turin", 
+            import_files={
+                'street_network':"./tests/test_data/turin_street_network.gpkg",
+                'trip_data':"./tests/test_data/turin_trips.csv",
+            },
+        )
+    )
+
+def test_growbikenet_case_success_offline_import_data3(validation_gdf_turin_alpha0_4):
+    """Verify that the offline version of growbikenet works as intended with import data sets.
+    """
+    gbn.settings.import_data_impact = 9
+    gbn.settings.import_data_trip_point_balance = 0.4 # alpha
+
+    validation_gdf_turin_alpha0_4.equals( 
+        gbn.growbikenet(
+            "Turin", 
+            export_data = True,
+            import_files={
+                'street_network':"./tests/test_data/turin_street_network.gpkg",
+                'point_data':"./tests/test_data/turin_crashes.gpkg",
+                'trip_data':"./tests/test_data/turin_trips.csv",
+            },
+        )
+    )
