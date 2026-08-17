6ebf23d5070065e24e8144630a23daef1bb9ca3d
Michael Szell <michael@szell.net>
2026-08-17 09:43:14 +0200

    Change import_path default setting to avoid reset

--- growbikenet/functions.py
+++ growbikenet/functions.py
@@ -421,7 +421,7 @@ def add_point_data_to_net(points, edges, crs_projected=constants._CRS_CALCULATIO
     return edges_with_data
 
 
-def import_network(growable_network, import_path=settings.import_path):
+def import_network(growable_network, import_path=None):
     """Import and project a street network from gpkg file.
 
     Parameters
@@ -455,6 +455,8 @@ def import_network(growable_network, import_path=settings.import_path):
     key 0.
     """
 
+    if import_path is None:
+        import_path = settings.import_path
     nodes = gpd.read_file(import_path+growable_network, layer='nodes')
     edges = gpd.read_file(import_path+growable_network, layer='edges')
 
