23d829433ba7c01f0e11556f709776b357c6e14a
Michael Szell <michael@szell.net>
2026-08-26 08:57:59 +0200

    Add css for dataframes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,6 +59,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_static_path = ["_static"]
+html_css_files = [
+    'css/dataframe.css',
+]
 
 ### select html theme
 html_theme = "furo"
